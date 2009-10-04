9432397efbc93191294102f853df547e96bcd033
Brian K. Buckley <briankbuckley@gmail.com>
2009-10-03 23:29:12 -0400

    added Integer#even?

--- 2/2.rb
+++ 2/2.rb
@@ -1,8 +1,14 @@
+class Integer
+  def even?
+    self % 2 == 0
+  end
+end
+
 sum = 0
-prev, fib = 1, 2
+fib_prev, fib = 1, 2
 while !(fib > 4000000) do
-  sum += fib if fib % 2 == 0
-  prev, fib = fib, prev + fib
+  sum += fib if fib.even?
+  fib_prev, fib = fib, fib_prev + fib
 end
 
 puts sum #4613732
