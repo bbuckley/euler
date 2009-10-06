c38434a20d7dfba8e37216a2b0ab27dcf5809a8a
Brian K. Buckley <briankbuckley@gmail.com>
2009-10-06 17:23:17 -0400

    13 added

--- 7/7.rb
+++ 7/7.rb
@@ -1,11 +1,6 @@
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6^(th) prime is 13.
 # 
 # What is the 10001^(st) prime number?
-# 
-# Answer:
-#   104743
 
 require 'mathn'
-x = Prime.instance.each
-10000.times { x.succ }
-puts x.succ #104743
+puts Prime.instance.each.tap{ |x| 10000.times{ x.next} }.next  #104743
