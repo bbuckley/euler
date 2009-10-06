f9be408cb2263091df13ff15b14fc4e3ce3ad958
Brian K. Buckley <briankbuckley@gmail.com>
2009-10-05 20:45:16 -0400

    5: by inspection

---
+++ 5/5.rb
@@ -0,0 +1,10 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+# 
+# What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
+# 
+# Answer:
+#   232792560s
+
+puts 2 * 3 * 2 * 5 * 7 * 2 * 3 * 11 * 13 * 2 * 17 * 19   #232792560
+
+
