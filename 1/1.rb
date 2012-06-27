6539daf8b1741dd8788d0ddbb5ffa936d65b408f
Brian K. Buckley <briankbuckley@gmail.com>
2012-06-27 20:39:23 -0300

    Update master

--- 1/1.rb
+++ 1/1.rb
@@ -4,7 +4,7 @@
 
 #1.upto(999).select{|e| [3,5].any?{|i| (e % i).zero? }}.inject(:+)  #233168
 
-# is thus better?
+# is this better?
 div = lambda{|x| [3,5].any?{|i| (x % i).zero? }
 1.upto(999).select(&div).inject(:+)
 
